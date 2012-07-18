369fea3f456bfb079fad45e57dec6b6c477e0a31
Javier Álvarez <xavijam@gmail.com>
2012-07-19 01:29:03 +0200

    hello backbone

--- js/app.js
+++ js/app.js
@@ -2,49 +2,105 @@
 
   (function() {
 
-    var map = new L.Map('map').setView(new L.LatLng(40.7143528, -74.0059731), 14)
-      , streetMapUrl = 'http://{s}.tiles.mapbox.com/v3/cartodb.map-u6vat89l/{z}/{x}/{y}.png'
-      , streetMapLayer = new L.TileLayer(streetMapUrl, { maxZoom: 19, attribution: 'Tiles: &copy; Mapbox' });
 
-    map.addLayer(streetMapLayer)
+    var NYCRestaurants = Backbone.View.extend({
 
-    var ny_restaurants = new L.CartoDBLayer({
-      map: map,
-      user_name:'viz2',
-      table_name: 'restaurant_week',
-      query: "SELECT * FROM {{table_name}}",
-      interactivity: "cartodb_id,name,meal,cuisine,price,link",
-      featureOver: function(ev,latlng,pos,data) {
-        // document.body.style.cursor = "pointer";
-        // showTooltip(data,pos)
+      el: $("body"),
+
+      options: {
+        user_name: "viz2",
+        table_name: "restaurant_week",
+        mapId: "map"
+      },
+
+      events: {
+        "click ul > li > a.type":  "_setFilter"
+      },
+
+
+      initialize: function() {
+        this._initMap();
+        this._initLayer();
+        this.filters = [];
+      },
+
+      render: function() {
         
-        // // Show the hover point if it is a different feature
-        // if (data.cartodb_id != circleMarker.cartodb_id) {
-        //   map.removeLayer(circleMarker);
+      },      
+
+
+      _initMap: function() {
+        var map = this.map = new L.Map(this.options.mapId).setView(new L.LatLng(40.7143528, -74.0059731), 14)
+          , mapboxUrl = 'http://{s}.tiles.mapbox.com/v3/cartodb.map-u6vat89l/{z}/{x}/{y}.png'
+          , mapboxLayer = new L.TileLayer(mapboxUrl, { maxZoom: 19, attribution: 'Tiles: &copy; Mapbox' });
+
+        map.addLayer(mapboxLayer);
+      },
+
+
+      _initLayer: function() {
+        var layer = this.layer = new L.CartoDBLayer({
+          map: this.map,
+          user_name: this.options.user_name,
+          table_name: this.options.table_name,
+          query: "SELECT * FROM {{table_name}}",
+          interactivity: "cartodb_id,name,meal,cuisine,price,link",
+          featureOver: this._featureOver,
+          featureOut: this._featureOut,
+          featureClick: this._featureClick,
+          auto_bound: false
+        });
+
+        this.map.addLayer(layer)
+      },
+
+      _updateLayer: function() {
+
+        var query = "SELECT * FROM {{table_name}} ";
+
+        if (_.size(this.filters) > 0) {
+          query += "WHERE ";
+          _.each(this.filters, function(cuisine,i) {
+            query += " cuisine ILIKE '%" + cuisine + "%' OR ";
+          });
+
+          query = query.substring(0, query.length-3);
+        }
         
-        //   circleMarker = new L.CircleMarker(new L.LatLng(data.lat, data.lng), hover_style);
-        //   circleMarker.cartodb_id = data.cartodb_id;
-          
-        //   map.addLayer(circleMarker);
+        this.layer.setQuery(query);
+      },
+
+
+      // Library events
+      _featureOver: function(ev,latlng,pos,data) {
+
+      },
+      _featureOut: function() {
+
+      },
+      _featureClick: function(ev,latlng,pos,data) {
+
+      },
+
+
+      // Aside events
+      _setFilter: function(ev) {
+        ev.preventDefault();
+
+        var cuisine = $(ev.target).text();
+
+        // if () {
+        //   $(ev.target).addClass("selected");
+        // } else {
+        //   $(ev.target).removeClass("selected");
         // }
-        
-      },
-      featureOut: function() {
-        // document.body.style.cursor = "default";
-        // hideTooltip();
-        
-        // // Hide and remove in any case the hover point
-        // circleMarker.cartodb_id = null;
-        // map.removeLayer(circleMarker)
-      },
-      featureClick: function(ev,latlng,pos,data) {
-        // console.log("jamon");
-        //window.open(data["link"],"_blank");
-      },
-      auto_bound: false
+
+        this.filters.push(cuisine);
+
+        // Change filter
+        this._updateLayer();
+      }
     });
 
-    map.addLayer(ny_restaurants)
-
-
+    var app = new NYCRestaurants();
   })()
